36aee7f1c10c2d0ea976e7e863be72ff6835e6e9
jade kim <kdm1171@gmail.com>
2021-07-21 01:33:24 +0900

    ch3 프로퍼티 확장

--- src/ch3/join.kt
+++ src/ch3/join.kt
@@ -17,13 +17,24 @@ fun <T> Collection<T>.joinToString(
     return result.toString()
 }
 
-fun String.lastChar(): Char = this.get(this.length - 1)
+//fun String.lastChar(): Char = this.get(this.length - 1)
+val String.lastChar: Char
+    get() = get(length - 1)
 
+var StringBuilder.lastChar: Char
+    get() = get(length - 1)
+    set(value: Char) {
+        this.setCharAt(length - 1, value)
+    }
 
 fun main(args: Array<String>) {
     val s = "Hello"
-    println(s.lastChar())
+    println(s.lastChar)
 
     val list = listOf(1, 2, 3)
     println(list.joinToString())
+
+    val sb = StringBuilder("Kotlin?")
+    sb.lastChar = '!'
+    println(sb)
 }

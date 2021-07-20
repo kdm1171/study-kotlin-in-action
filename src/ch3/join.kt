1056e0cf3b1754517659c99e77aa9e74bdb07bc3
jade kim <kdm1171@gmail.com>
2021-07-21 00:18:29 +0900

    ch3 joinToString 최종

--- src/ch3/join.kt
+++ src/ch3/join.kt
@@ -3,14 +3,13 @@
 package strings
 
 @JvmOverloads
-fun <T> joinToString(
-    collection: Collection<T>,
+fun <T> Collection<T>.joinToString(
     separator: String = ", ",
     prefix: String = "",
     postfix: String = ""
 ): String {
     val result = StringBuilder(prefix)
-    for ((index, element) in collection.withIndex()) {
+    for ((index, element) in withIndex()) {
         if (index > 0) result.append(separator)
         result.append(element)
     }
@@ -24,4 +23,7 @@ fun String.lastChar(): Char = this.get(this.length - 1)
 fun main(args: Array<String>) {
     val s = "Hello"
     println(s.lastChar())
+
+    val list = listOf(1, 2, 3)
+    println(list.joinToString())
 }

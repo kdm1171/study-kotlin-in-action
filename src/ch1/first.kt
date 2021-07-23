88f0a67f6f366a7544ceb1597d6c1f3e984bf0d5
김동민 <kdm1171@naver.com>
2021-07-23 23:15:09 +0900

    maxBy method deprecated in kotlin 1.5

--- src/ch1/first.kt
+++ src/ch1/first.kt
@@ -5,7 +5,7 @@ data class Person(val name: String, val age: Int? = null)
 fun main(args: Array<String>) {
     val persons = listOf(Person("jade", age = 35), Person("kim", age = 34))
 
-    val oldest = persons.maxBy { it.age ?: 0 }
+    val oldest = persons.maxByOrNull{ it.age ?: 0 }
 
     println("oldest person : $oldest")
 
